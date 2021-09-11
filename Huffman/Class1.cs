33e5acd96cc3fe2dae2e9f8ad9b217be499993ea
anzucel <andreazucely70@gmail.com>
2021-09-11 17:36:37 -0600

    Agregar archivos de proyecto.

---
+++ Huffman/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Huffman
+{
+    public class Class1
+    {
+    }
+}
---
+++ Huffman/IHuffman.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Huffman
+{
+    interface IHuffman
+    {
+        void Comprimir(string texto);
+        void Descomprimir(string texto);
+    }
+}
