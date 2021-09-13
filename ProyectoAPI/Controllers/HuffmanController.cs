76376acd576ea0b16a2d511835a8042d34fd8dec
JasonZamora <67882782+JasonZamora@users.noreply.github.com>
2021-09-13 17:39:47 -0600

    Se agrego los metodos post y get. Se agrego parte del código para leer los archivos

---
+++ ProyectoAPI/Controllers/HuffmanController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoAPI.Models;
+using Microsoft.Extensions.Logging;
+
+namespace ProyectoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Huffman : ControllerBase
+    {
+        // GET: api/<Huffman> 
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1" };
+        }
+
+
+
+        [HttpGet]
+        [Route("compressions")]
+        public IEnumerable<string> Getcompressions()
+        {
+            return new string[] { };
+        }
+
+
+        // POST api/<Huffman>
+
+        [HttpPost]
+        [Route("decompress")]
+        public IActionResult PostFileDecompress([FromForm] IFormFile File)
+        {
+            using var archivo = new MemoryStream();
+            try
+            {
+                File.CopyToAsync(archivo);
+                var coleccion = Encoding.ASCII.GetString(archivo.ToArray());
+                //foreach (var nuevo in listaPeliculas)
+                //{
+                //    Singleton.Instance.ABPeliculas.Insertar(nuevo);
+                //}
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+
+
+        [HttpPost]
+        [Route("compress/{name}")]
+        public IActionResult PostFileCompress([FromForm] IFormFile File, [FromRoute] string name)
+        {
+            using var archivo = new MemoryStream();
+            try
+            {
+                File.CopyToAsync(archivo);
+                var coleccion = Encoding.ASCII.GetString(archivo.ToArray());              
+                //foreach (var nuevo in listaPeliculas)
+                //{
+                //    Singleton.Instance.ABPeliculas.Insertar(nuevo);
+                //}
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+      
+    }
+}
---
+++ ProyectoAPI/Models/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProyectoAPI;
+using ProyectoAPI.Models;
+
+namespace ProyectoAPI.Models
+{
+    public sealed class Singleton
+    {
+        private readonly static Singleton instance = new Singleton();
+
+        private Singleton()
+        {
+           
+        }
+      
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
